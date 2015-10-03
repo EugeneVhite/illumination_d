aa069182ded56ed2fa2e5f26a289653dc6eb6bba
EugeneVhite <eugene2494@yahoo.com>
2015-10-03 14:09:35 +0300

    new currency helper for products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def to_russian_currency(value)
+    number_to_currency value, unit: '₽ ', delimiter: ''
+  end
+
+  def first_two_words_of(string)
+    string.split[0...2].join(' ')
+  end
 end
